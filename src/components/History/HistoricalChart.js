ee23768160a195d2bdc47554f292d301827feb08
eonsang <2eonhworking@gmail.com>
2021-02-01 05:27:51 +0900

    feat: img chart

---
+++ src/components/History/HistoricalChart.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { Bar } from 'react-chartjs-2'
+
+
+
+const HistoricalChart = ({ historycalPerfomanceWorks }) => {
+  console.log(historycalPerfomanceWorks);
+  const data = {
+    labels: historycalPerfomanceWorks.map(history => history.date),
+    datasets: [
+      {
+        type: 'line',
+        data: historycalPerfomanceWorks.map(history => history.price),
+        fill: false,
+        borderDash: [5],
+        borderColor: '#FFEBCC',
+        pointBackgroundColor: '#FFEBCC',
+        pointBorderWidth: '10px'
+      },
+    ],
+  }
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: false,
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      yAxes: [
+        {
+          position: 'right',
+          ticks: {
+            fontColor: '#639DB7',
+            beginAtZero: true,
+            callback: function(value) {
+              return '$' + value + 'M';
+            }
+          },
+        },
+      ],
+    },
+  }
+  return (
+    <>
+      <Bar data={data} options={options} />
+    </>
+  )
+}
+
+export default HistoricalChart
--- src/components/History/index.js
+++ src/components/History/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Section from "../Layout/Section";
 import Wrap from "../Layout/Wrap";
+import HistoricalChart from "./HistoricalChart";
 
 const History = ({historycalPerfomanceWorks}) => {
   return (
@@ -9,8 +10,19 @@ const History = ({historycalPerfomanceWorks}) => {
       <Section>
         <Wrap>
           <h2>Historical Performance of Similar Works</h2>
-          <div>
-            charts...
+          <div className={'historical-chart'}>
+            <HistoricalChart historycalPerfomanceWorks={historycalPerfomanceWorks} />
+            <div className={'historical-imgs'}>
+              {historycalPerfomanceWorks.map(history => {
+                return (
+                  <div className={'imgs'} key={history.seq}>
+                    <span>
+                      <img src={history.img} alt=""/>
+                    </span>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </Wrap>
       </Section>
@@ -25,6 +37,42 @@ const HistoryLayout = styled.div`
     color:#fff;
     margin-bottom: 10px;
     font-weight: 600;
+  }
+  
+  .historical-imgs {
+    display: flex;
+    align-items: center;
+    padding-right: 50px;
+    .imgs {
+      flex: 1;
+      padding: 5px;
+      span {
+        margin: 0 auto;
+        background:#0C435B;
+        display: block;
+        height: 70px;
+        width: 70px;
+        @media screen and (max-width: 768px) {
+          height: 45px;
+          width: 45px;
+        }
+      }
+    }
+  }
+  .historical-chart {
+    background: url('/assets/images/historical-bg.png') no-repeat center top;
+    background-size: 100% auto;
+    padding: 0 9%;
+    @media screen and (max-width: 1470px) {
+      background: none;
+      padding: 0;
+    }
+    canvas {
+      height: 390px !important;
+      @media screen and (max-width: 768px) {
+        height: 250px !important;  
+      }
+    }
   }
 `;
 
